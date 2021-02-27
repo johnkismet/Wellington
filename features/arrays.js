1440d5ee879192452a73fa5578c056375d9eadf7
johnkismet <mojomanftw@gmail.com>
2021-02-27 17:26:33 -0500

    Made the responses more interesting, and added a very rare chance to get called out if you aren't funny

---
+++ features/arrays.js
@@ -0,0 +1,126 @@
+export const code_arr = [
+	{
+		file: "https://i.imgur.com/hAjzFLW.png",
+		text: "const animals = ['pigs', 'goats', 'sheep'];",
+	},
+	{
+		file: "https://i.imgur.com/hieruOX.png",
+		text: "const solve = m => 1 - ((4*m + 1)**.5 - 1) / (2*m);",
+	},
+	{
+		file: "https://i.imgur.com/OZXdLeX.png",
+		text:
+			"deg, g, q = Counter(), defaultdict(list), set(range(len(adj_matrix)))",
+	},
+	{
+		file: "https://i.imgur.com/SOMbQg5.png",
+		text: "var lat = (parseInt(r[1]) + parseInt(r[2])/60",
+	},
+	{
+		file: "https://i.imgur.com/Dmb2adM.png",
+		text:
+			"Math.pow(Math.sin(dlat/2), 2) + Math.cos(coord1.lat) * Math.cos(coord2.lat)",
+	},
+	{
+		file: "https://i.imgur.com/GBUkSfs.png",
+		text: "diffLat = lat2 - lat1, diffLon = lon2 - lon1",
+	},
+	{
+		file: "https://i.imgur.com/t3lOMOy.png",
+		text: "const {acos,sin,cos} = Math;",
+	},
+	{
+		file: "https://i.imgur.com/6MeZW0Q.png",
+		text: "str.replace(/[^01 ]/g, '').split(/ +/).sort((a, b) => a - b).pop()",
+	},
+	{
+		file: "https://i.imgur.com/fWcyGnh.png",
+		text: "string.match(/[01]+([^s01]*[01])*/g)",
+	},
+	{
+		file: "https://i.imgur.com/GKyxpt3.png",
+		text: "var regExp=/[-_]w/ig;",
+	},
+	{
+		file: "https://i.imgur.com/jEShggd.png",
+		text: "for (int i = 0; str[i] != '\0'; ++i)",
+	},
+	{
+		file: "https://i.imgur.com/UMrLlbT.png",
+		text: "if (x >= 0 && y >= 0 && x >  y) return x * (3 * x + 1) + y;",
+	},
+	{
+		file: "https://i.imgur.com/kLHQF8Q.png",
+		text:
+			"byRecipe[ingredient].forEach(([i, q]) => stack.push([i, q * quantity]));",
+	},
+	{
+		file: "https://i.imgur.com/dMKck3z.png",
+		text: "const getMonthDays = (y) => (isLeap(y) ? md.l : md.s);",
+	},
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+	// {
+	// 	file: "../assets/race_images/",
+	// 	text: "",
+	// },
+];
+export const greetings = [
+	"Absolutely, here is the text for your race:",
+	"I will get right on that. Here is your text:",
+	"You may now begin the race",
+	"Your wish is my command",
+	"Yes, master. I've found some text for you:",
+	"3...2...1...Begin",
+	"Race!",
+];
--- features/codeRace.js
+++ features/codeRace.js
@@ -1,15 +1,11 @@
-// TODO: Import code race function here
+import { code_arr, greetings } from "./arrays";
 export default function mainRace(message) {
-	let code_arr = [
-		"Hello there",
-		"This is a sentence",
-		"Type me",
-		"blah blah blur",
-		"baba booey",
-		"badda bing badda boom",
-		"Get laid, get paid, gatorade",
-	];
 	let chosenText = code_arr[Math.floor(Math.random() * code_arr.length)];
-	message.channel.send(`Let's race! Here is the text to type: ${chosenText}`);
-	return chosenText;
+	message.channel.send(
+		greetings[Math.floor(Math.random() * greetings.length)],
+		{
+			files: [chosenText.file],
+		}
+	);
+	return chosenText.text;
 }
