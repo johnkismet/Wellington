a1a5881c01a85e553973c036b900e2a9940366fb
johnkismet <mojomanftw@gmail.com>
2021-02-27 15:21:40 -0500

    Bot is basically functional, now to give it actual content

--- features/codeRace.js
+++ features/codeRace.js
@@ -1,1 +1,15 @@
 // TODO: Import code race function here
+export default function mainRace(message) {
+	let code_arr = [
+		"Hello there",
+		"This is a sentence",
+		"Type me",
+		"blah blah blur",
+		"baba booey",
+		"badda bing badda boom",
+		"Get laid, get paid, gatorade",
+	];
+	let chosenText = code_arr[Math.floor(Math.random() * code_arr.length)];
+	message.channel.send(`Let's race! Here is the text to type: ${chosenText}`);
+	return chosenText;
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import mainRace from "./features/codeRace";
+
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -7,17 +9,34 @@ const prefix = "!";
 client.once("ready", () => {
 	console.log("Sir Wellington is at your service ;)");
 });
-
+let raceStarted = false;
+let raceText = null;
 client.on("message", (message) => {
+	if (raceStarted && !message.author.bot && message.content !== "!race") {
+		if (message.content === raceText) {
+			message.reply("You win!");
+			raceText = null;
+			raceStarted = false;
+		} else {
+			message.reply(`Not right!`);
+		}
+	}
+
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === "race") {
-		message.channel.send("Here's your image", {
-			files: ["./assets/race_images/carbon.png"],
-		});
+	if (command === "race" && !raceStarted) {
+		// message.channel.send("Here's your image", {
+		// 	files: ["./assets/race_images/carbon.png"],
+		// });
+		raceText = mainRace(message);
+		raceStarted = true;
+	} else if (raceStarted) {
+		message.channel.send(
+			`A race has already been started. The text is ${raceText}`
+		);
 	}
 });
 
